a7cb39228ee2b4d3899471a96dd1c45fc8f06f6c
Jorge Garcia <garcijo94@gmail.com>
2016-12-15 11:07:36 -0500

    Adding base files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Nailed It
+                    Laravel
                 </div>
 
                 <div class="links">
